272b5bb9533d6630665d725627d8a89bf9a0f2b1
George Hickman <george@ghickman.co.uk>
2011-12-18 08:36:32 +0000

    remove testing items

--- external.py
+++ external.py
@@ -2,7 +2,7 @@ import urllib2
 import json
 
 # define a Python data dictionary
-data = {'message': 'burritos are here', 'host': '192.168.0.7', 'os': 'lion'}
+data = {'message': '', 'host': '', 'os': '', 'password': ''}
 data_json = json.dumps(data)
 host = 'http://localhost:5000/growl'
 req = urllib2.Request(host, data_json, {'content-type': 'application/json'})
